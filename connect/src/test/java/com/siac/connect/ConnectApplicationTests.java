61ef2feff97547e4616bcd4dc45e4482bc81ca79
tiagoribeironfs1 <tiagoribeironfs1@gmail.com>
2024-04-16 14:05:50 -0300

    feat: rename test unit

--- connect/src/test/java/com/siac/connect/ConnectApplicationTests.java
+++ connect/src/test/java/com/siac/connect/ConnectApplicationTests.java
@@ -1,10 +1,10 @@
-package com.siac.conncect;
+package com.siac.connect;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConncectApplicationTests {
+class ConnectApplicationTests {
 
 	@Test
 	void contextLoads() {
