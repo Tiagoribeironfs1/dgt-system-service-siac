cc6fc5275a532ae01db147e657b68aa76a1085ce
tiagoribeironfs <49324313+Tiagoribeironfs1@users.noreply.github.com>
2024-04-16 09:49:06 -0300

    Updated logging to use SLF4J for better management.

--- connect/src/main/java/com/siac/ConnectApplication.java
+++ connect/src/main/java/com/siac/ConnectApplication.java
@@ -3,19 +3,21 @@ package com.siac;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
 import com.siac.service.ConnectSiacService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class ConnectApplication {
 
-	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(ConnectApplication.class, args);
-		
-		ConnectSiacService connectSiacService = context.getBean(ConnectSiacService.class);
-		connectSiacService.realizarOperacoesBancoDados();
-		System.out.println("Service connect start localhost:8081");
-	}
-	
+    private static final Logger log = LoggerFactory.getLogger(ConnectApplication.class);
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(ConnectApplication.class, args);
+        ConnectSiacService connectSiacService = context.getBean(ConnectSiacService.class);
+        connectSiacService.realizarOperacoesBancoDados();
+        log.info("Service connect start localhost:8081");
+    }
+
 }
 
